29c2d36ef4c520ad64aab80a9297dc4ff07797a5
LieJiang <jianglie@vip.qq.com>
2016-10-14 17:46:40 +0800

    merge

---
+++ survey_Common/src/main/java/com/fire/entites/EmployeeTemp.java
@@ -0,0 +1,28 @@
+package com.fire.entites;
+
+public class EmployeeTemp {
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeTemp [id=" + id + ", name=" + name + "]";
+	}
+
+}
